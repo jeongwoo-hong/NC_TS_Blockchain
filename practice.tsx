b997b790d1cfb6f64139f1ed776a90101754910c
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-23 23:38:04 +0900

    22.12.23.7

--- practice.tsx
+++ practice.tsx
@@ -21,3 +21,21 @@ const add2 = (a:number, b:number) => a + b
 type Add = (a:number, b:number) => number;
 
 const add3:Add = (a, b) => a + b
+
+type Config = {
+    path: string,
+    state: object
+}
+
+type Push = {
+    (path:string):void
+    (obj:Config):void
+}
+
+const push:Push = (config) => {
+    if(typeof config === "string") console.log(config)
+    else {
+        console.log(config.path)
+    }
+}
+
