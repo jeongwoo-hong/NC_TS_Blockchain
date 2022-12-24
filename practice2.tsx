79d6709cf09a56b4dc62267e79378a23c0abe996
jeongwoo-hong <ghdepddn@snu.ac.kr>
2022-12-24 17:35:14 +0900

    22.12.24.10

--- practice2.tsx
+++ practice2.tsx
@@ -1,17 +1,19 @@
-abstract class User3 {
-    constructor(
-        protected firstName:string,
-        protected lastName:string
-    ) {}
-    abstract sayHi(name:string):string
-    abstract fullName():string
+interface User3 {
+    firstName:string,
+    lastName:string,
+    sayHi(name:string):string
+    fullName():string
 }
 
-class Player3 extends User3 {
-    getFullName(): string {
-        return `${this.firstName} ${this.lastName}`
-    }
-    sayHi(name:string) {
-        return `Hello ${name}. my name is ${this.fullName}`
-    }
+class Player3 implements User3 {
+    constructor(
+        public firstName:string,
+        public lastName:string
+    ) {}
+        fullName(): string {
+            return `${this.firstName} ${this.lastName}`
+        }
+        sayHi(name: string){
+            return `Hello ${name}. my name is ${this.fullName()}`
+        }
 }
